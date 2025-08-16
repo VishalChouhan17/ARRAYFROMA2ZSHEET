bf936606f7c1a1bc13f87d537ff9dc94238f7024
VishalChouhan17 <vishal.chouhan2028@gmail.com>
2025-08-16 12:07:43 +0530

    Delete learndsa folder

--- learndsa/newfile.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-int arr[]={3,2,1,2,3,6,1};
-int n=7;
-int tg=10;
-cout<<arr[0];
-}
--- learndsa/sum.cpp
+++
@@ -1,5 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int m=3;
-int n=3;
-cout<<n+m<<endl;
